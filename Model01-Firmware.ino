4fd3a7e566992241486e40360cfb85d91e812174
Ross Reinhardt <rreinhardt9@gmail.com>
2018-05-12 23:57:08 -0400

    Set up one shot and modifier highlights

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -28,6 +28,13 @@
 // Support for "Numpad" mode, which is mostly just the Numpad specific LED mode
 #include "Kaleidoscope-NumPad.h"
 
+// One Shot
+#include "Kaleidoscope-OneShot.h"
+#include "Kaleidoscope-Escape-OneShot.h"
+
+// Modifier highlights
+#include "Kaleidoscope-LED-ActiveModColor.h"
+
 // Support for an "LED off mode"
 #include "LED-Off.h"
 
@@ -153,15 +160,15 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_F, Key_P, Key_G, Key_Tab,
    Key_PageUp,   Key_A, Key_R, Key_S, Key_T, Key_D,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
-   ShiftToLayer(FUNCTION),
+   OSM(LeftControl), Key_Backspace, OSM(LeftGui), OSM(LeftShift),
+   OSL(FUNCTION),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_J, Key_L, Key_U,     Key_Y,         Key_Semicolon, Key_Equals,
                   Key_H, Key_N, Key_E,     Key_I,         Key_O,         Key_Quote,
    Key_RightAlt,  Key_K, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightControl,
-   ShiftToLayer(FUNCTION)),
+   OSM(RightShift), OSM(LeftAlt), Key_Spacebar, OSM(RightControl),
+   OSL(FUNCTION)),
 
   [NUMPAD] =  KEYMAP_STACKED
   (___, ___, ___, ___, ___, ___, ___,
@@ -323,6 +330,12 @@ void setup() {
 
     // LEDControl provides support for other LED modes
     &LEDControl,
+
+    // One Shot
+    &OneShot, &EscapeOneShot,
+
+    // Modifier highlights
+    &ActiveModColorEffect,
 
     // We start with the LED effect that turns off all the LEDs.
     &LEDOff,
